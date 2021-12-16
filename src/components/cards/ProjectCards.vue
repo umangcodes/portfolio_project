77ed00355082bcc12a754b2d0c5f3a3f686d2faa
Umang A <umanga.eng@gmail.com>
2021-12-16 13:41:57 -0500

    project cards created. modifications to include data of the composed projects to be added soon. reusable component

---
+++ src/components/cards/ProjectCards.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="h-96 w-80">Title: {{ title }}</div>
+</template>
+
+<script>
+export default {
+  props: ["id", "title", "description", "gitLink", "deplink"],
+};
+// gitlink --> github link to project
+// deplink --> deployment link to project
+</script>
+
+<style lang="scss" scoped></style>
