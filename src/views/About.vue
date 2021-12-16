192cdfa4b79a5ddfbf244e8fc45fc66622861918
Umang A <umanga.eng@gmail.com>
2021-12-15 22:14:49 -0500

    about component modified to include about information

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,17 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div id="about" class="h-screen">
+    <div class="flex flex-col justify-center">
+      <span class="italic text-3xl font-thin text-center"
+        >About this portfolio</span
+      >
+      <p class="text-lg font-extralight p-5 text-center">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente
+        perferendis est, eveniet perspiciatis totam autem! Delectus, sit
+        corrupti inventore eveniet quo maxime impedit incidunt consectetur
+        facilis qui vitae nostrum sint rerum numquam iure suscipit similique
+        iste ipsa ea obcaecati doloribus vel error. Dolorem quo dolor
+        perspiciatis pariatur, nam quaerat aliquid!
+      </p>
+    </div>
   </div>
 </template>
