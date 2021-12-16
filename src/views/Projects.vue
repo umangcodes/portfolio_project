fbc3d06c1f58170957ec80a2701b43b78f9f065e
Umang A <umanga.eng@gmail.com>
2021-12-16 13:43:08 -0500

    new view file created to include all the portfolio projects

---
+++ src/views/Projects.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="projects" class="mx-5 my-5 h-screen">
+    <!-- Heading -->
+    <div class="flex justify-center">
+      <span class="text-4xl">My projects</span>
+    </div>
+    <!-- Project Cards -->
+    <div class="">
+      Project cards
+      <projectCards :title="name" />
+    </div>
+  </div>
+</template>
+
+<script>
+import ProjectCards from "../components/cards/ProjectCards.vue";
+export default {
+  data() {
+    return {
+      
+    };
+  },
+  components: {
+    ProjectCards,
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
