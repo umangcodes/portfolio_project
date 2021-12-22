0b632f3256969692fe86d4255cc656ff66463e51
Umang A <umanga.eng@gmail.com>
2021-12-21 21:56:50 -0500

    secret path disabled for users without password

--- src/store/index.js
+++ src/store/index.js
@@ -2,7 +2,7 @@ import { createStore } from "vuex";
 
 export default createStore({
   state: {
-    passcode: "pass!",
+    passcode: "pass",
   },
   mutations: {
     SET_PASSCODE(state, payload) {
