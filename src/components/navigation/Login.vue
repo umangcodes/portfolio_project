c5354722e359342a66e9641fea8d19dd4166bce5
Umang A <umanga.eng@gmail.com>
2021-12-21 22:34:38 -0500

    added UI for password field to the secret path

--- src/components/navigation/Login.vue
+++ src/components/navigation/Login.vue
@@ -6,8 +6,19 @@
         class="flex flex-col mx-5 my-5 items-center"
       >
         <Label>Want to know more?</Label>
-        <input type="text" v-model="password" />
-        <button></button>
+        <span class="text-center"
+          >Enter a passcode and you will be allowed into the amazing world I
+          live in!</span
+        >
+        <div class="mt-5">
+          <input class="border-b mx-5" type="text" v-model="password" />
+          <button
+            :click="login"
+            class="border rounded-full px-2 italic font-semibold hover:shadow-lg"
+          >
+            Go!
+          </button>
+        </div>
       </form>
     </div>
   </div>
