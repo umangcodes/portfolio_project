c12c2740fbd4bddd59567259fe756c1c14051b9b
Umang A <umanga.eng@gmail.com>
2021-12-21 21:55:52 -0500

    secret path along with secials component optimized for mobile view

--- src/components/secret/Specials.vue
+++ src/components/secret/Specials.vue
@@ -1,7 +1,7 @@
 <template>
-  <div id="specials" class="h-screen mx-5 my-10">
-    <div class="grid grid-cols-2">
-      <div class="grid-start-1 flex flex-col items-center border-r">
+  <div id="specials" class="h-auto mx-5 my-10">
+    <div class="flex flex-col lg:grid lg:grid-cols-2">
+      <div class="grid-start-1 flex flex-col items-center lg:border-r">
         <span class="text-9xl">Img</span>
       </div>
       <div class="grid-start-2 mx-5 flex h-48 items-center">
@@ -14,8 +14,11 @@
       </div>
     </div>
 
-    <div class="grid grid-cols-2 mt-40">
-      <div class="grid-start-1 mx-5 flex h-48 items-center">
+    <div class="flex flex-col lg:grid lg:grid-cols-2 mt-40">
+      <div class="col-start-2 flex flex-col items-center lg:border-l">
+        <span class="text-9xl">Img</span>
+      </div>
+      <div class="col-start-1 row-start-1 mx-5 flex h-48 items-center">
         <span class=""
           >Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium
           exercitationem natus repellat esse consequatur saepe doloribus, quasi
@@ -23,12 +26,9 @@
           eos.</span
         >
       </div>
-      <div class="grid-start-2 flex flex-col items-center border-r">
-        <span class="text-9xl">Img</span>
-      </div>
     </div>
 
-    <div class="grid grid-cols-2 mt-40">
+    <div class="flex flex-col lg:grid lg:grid-cols-2 mt-40">
       <div class="grid-start-1 flex flex-col items-center border-r">
         <span class="text-9xl">Img</span>
       </div>
--- src/views/SecretPathV1.vue
+++ src/views/SecretPathV1.vue
@@ -2,27 +2,34 @@
   <div id="discover_secret">
     <div class="h-96 mt-40">
       <div class="flex flex-col items-center">
-        <span class="text-4xl font-semibold">Discover more about me here</span>
-        <span class="font-thin"
-          >Congratulations on finding the secret path. I appreciate your effort
-          and welcome to the secret page.
+        <span class="text-4xl font-semibold text-center mx-5 my-5"
+          >Discover more</span
+        >
+        <span class="font-thin text-center mx-5"
+          >Congratulations on finding the secret path. <br />I appreciate your
+          effort and welcome to the secret page.
         </span>
       </div>
     </div>
     <div class="my-5 flex justify-center">
-      <span class=""
-        >Discover more about what I do apart from web development</span
+      <span class="mx-5 my-5 text-center text-2xl italic"
+        >Discover more about what I do apart from web development!</span
       >
     </div>
-    <Specials />
+    <div
+      class="flex flex-col font-bold justify-center items-center text-xl mx-5 my-5 h-screen border-4 bg-gray-400 text-gray-200"
+    >
+      Coming very soon!
+    </div>
+    <!-- <Specials /> -->
   </div>
 </template>
 
 <script>
-import Specials from "../components/secret/Specials.vue";
+// import Specials from "../components/secret/Specials.vue";
 export default {
   components: {
-    Specials,
+    // Specials,
   },
   methods: {
     scrollToTop() {
