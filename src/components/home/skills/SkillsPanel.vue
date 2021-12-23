907ba0667a25f5e05c2d43900bf92b9e09cc178e
Umang A <umanga.eng@gmail.com>
2021-12-23 17:41:44 -0500

    new skills panel created

---
+++ src/components/home/skills/SkillsPanel.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="h-auto lg:grid lg:grid-cols-3 flex flex-col px-2">
+    <div class="lg:col-start-1 lg:px-2 py-2">
+      <img
+        src="../../../assets/cover-image.jpg"
+        class="md:-skew-y-6 transform-gpu"
+        alt=""
+      />
+    </div>
+    <div class="lg:col-start-2 lg:px-2 pb-2 -mt-4">
+      <img src="../../../assets/cover-image.jpg" class="hover:scale" alt="" />
+    </div>
+    <div class="lg:col-start-3 lg:px-2 py-2 hover:cursor-pointer">
+      <img
+        class="md:skew-y-6 transform-gpu"
+        src="../../../assets/cover-image.jpg"
+        alt=""
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped></style>
