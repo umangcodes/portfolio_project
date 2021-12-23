6856f24fef0174110fbb82acc2e5babdc25e7f55
Umang A <umanga.eng@gmail.com>
2021-12-22 23:08:15 -0500

    custom 404 page created

---
+++ src/components/errors/error404.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="h-screen flex flex-col items-center justify-center">
+    <span class="text-3xl">I am not sure what you are looking for!</span>
+    <span class="text-xl font-thin">Page does not exists...</span>
+    <router-link to="/" class="border px-3 py-2 rounded-full hover:shadow-lg"
+      >Home</router-link
+    >
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped></style>
