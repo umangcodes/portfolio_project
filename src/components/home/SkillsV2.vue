794ed33b0aa70459e4076e55f3da509f34db81c7
Umang A <umanga.eng@gmail.com>
2021-12-23 17:39:07 -0500

    new skills panel

---
+++ src/components/home/SkillsV2.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="h-screen border mx-5 my-5">
+    <div class="flex flex-col">
+      <span class="text-center text-2xl py-5 font-semibold">Skills</span>
+      <div>
+        <SkillsPanel />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import SkillsPanel from "../../components/home/skills/SkillsPanel.vue";
+export default {
+  components: {
+    SkillsPanel,
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
