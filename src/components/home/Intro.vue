c024b2dbcd8a5119dd792311ebfc06d193217e3a
Umang A <umanga.eng@gmail.com>
2021-12-28 20:45:13 -0500

    height changed. text moved to center by change in margins

--- src/components/home/Intro.vue
+++ src/components/home/Intro.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="intro">
     <!-- class="flex h-screen items-center m-0" -->
-    <div class="flex flex-col justify-center my-12 h-96">
+    <div class="flex flex-col justify-center -my-24 h-screen">
       <span class="text-3xl text-center font-semibold">Hi, I'm Umang.</span>
       <span class="text-center text-lg font-thin"
         >Web developer, Control systems Engineer, Aviation enthusiast and much
