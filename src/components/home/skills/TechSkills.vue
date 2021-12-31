ab012594e02587da101d13658e21c63c51aa3009
Umang A <umanga.eng@gmail.com>
2021-12-31 11:53:00 -0500

    icons updated, techskills panel created, todo updated

---
+++ src/components/home/skills/TechSkills.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="lg:grid lg:grid-cols-3 flex">
+    <div class="lg:col-start-2 flex justify-around">
+      <TechSkillsPanel />
+    </div>
+  </div>
+</template>
+
+<script>
+import TechSkillsPanel from "../skills/TechSkillsPanel.vue";
+export default {
+  components: {
+    TechSkillsPanel,
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/components/home/skills/TechSkillsPanel.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="grid grid-cols-5 gap-5">
+    <div class="col-start-1">
+      <img :src="require('@/assets/icons/vue.svg')" alt="" class="h-16" />
+    </div>
+    <div class="col-start-2">
+      <img :src="require('@/assets/icons/html.svg')" alt="" class="h-16" />
+    </div>
+    <div class="col-start-3">
+      <img :src="require('@/assets/icons/css.svg')" alt="" class="h-16" />
+    </div>
+    <div class="col-start-4">
+      <img :src="require('@/assets/icons/react.svg')" alt="" class="h-16" />
+    </div>
+    <div class="col-start-5">
+      <img :src="require('@/assets/icons/linux.svg')" alt="" class="h-16" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
