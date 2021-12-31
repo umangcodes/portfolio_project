93052e55651e85e25087978baeeb843b1153a735
Umang A <umanga.eng@gmail.com>
2021-12-31 11:45:41 -0500

    skills panel updated

--- src/components/home/SkillsV2.vue
+++ src/components/home/SkillsV2.vue
@@ -1,19 +1,23 @@
 <template>
-  <div class="h-screen border mx-5 my-5">
+  <div class="h-96 mx-5 my-5">
     <div class="flex flex-col">
       <span class="text-center text-2xl py-5 font-semibold">Skills</span>
       <div>
-        <SkillsPanel />
+        <!-- <SkillsPanel /> -->
+        <TechSkills />
       </div>
+      <span class="text-xl text-center py-5"> & much more...</span>
     </div>
   </div>
 </template>
 
 <script>
-import SkillsPanel from "../../components/home/skills/SkillsPanel.vue";
+// import SkillsPanel from "../../components/home/skills/SkillsPanel.vue";
+import TechSkills from "../../components/home/skills/TechSkills";
 export default {
   components: {
-    SkillsPanel,
+    // SkillsPanel,
+    TechSkills,
   },
 };
 </script>
