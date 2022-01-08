175f644bfdf4a6c33cde5fac5a5f8cacdce0f84e
Umang A <umanga.eng@gmail.com>
2022-01-08 16:25:14 -0500

    added navigation button to the carousel

--- src/views/PersonalCard.vue
+++ src/views/PersonalCard.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="flex flex-col justify-center items-center h-screen w-auto">
-    <div class="flex">
+  <div class="flex flex-col justify-center items-center h-screen">
+    <div class="flex w-4/5 justify-evenly">
       <button
         :class="
           disablePrev
