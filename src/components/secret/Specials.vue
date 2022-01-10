a1bc0bbff9eee75333369a924171788d5feb5549
Umang A <umanga.eng@gmail.com>
2022-01-10 15:59:28 -0500

    specials changed to dynamic component. props available

--- src/components/secret/Specials.vue
+++ src/components/secret/Specials.vue
@@ -1,51 +1,50 @@
 <template>
-  <div id="specials" class="h-auto mx-5 my-10">
-    <div class="flex flex-col lg:grid lg:grid-cols-2">
-      <div class="grid-start-1 flex flex-col items-center lg:border-r">
-        <span class="text-9xl">Img</span>
+  <div>
+    <div
+      v-if="index % 2 != 0"
+      class="flex flex-col lg:grid lg:grid-cols-2 mt-40"
+    >
+      <div class="grid-start-2 flex flex-col items-center lg:border-l">
+        <span class="text-9xl">{{ dataObject.title }}</span>
       </div>
-      <div class="grid-start-2 mx-5 flex h-48 items-center">
-        <span class=""
-          >Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium
-          exercitationem natus repellat esse consequatur saepe doloribus, quasi
-          beatae quia perferendis, soluta deserunt sapiente repellendus
-          eos.</span
+      <div
+        class="grid-start-1 row-start-1 mx-5 flex flex-col h-48 items-center"
+      >
+        <span class="">{{ dataObject.shortDescription }}</span>
+        <button
+          class="m-auto rounded-full border w-auto px-3 py-2 hover:shadow-md"
+          :onClick="() => $router.push({ name: dataObject.link })"
         >
+          Visit Page
+        </button>
       </div>
     </div>
-
-    <div class="flex flex-col lg:grid lg:grid-cols-2 mt-40">
-      <div class="col-start-2 flex flex-col items-center lg:border-l">
-        <span class="text-9xl">Img</span>
-      </div>
-      <div class="col-start-1 row-start-1 mx-5 flex h-48 items-center">
-        <span class=""
-          >Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium
-          exercitationem natus repellat esse consequatur saepe doloribus, quasi
-          beatae quia perferendis, soluta deserunt sapiente repellendus
-          eos.</span
-        >
-      </div>
-    </div>
-
-    <div class="flex flex-col lg:grid lg:grid-cols-2 mt-40">
-      <div class="grid-start-1 flex flex-col items-center border-r">
-        <span class="text-9xl">Img</span>
-      </div>
-      <div class="grid-start-2 mx-5 flex h-48 items-center">
-        <span class=""
-          >Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium
-          exercitationem natus repellat esse consequatur saepe doloribus, quasi
-          beatae quia perferendis, soluta deserunt sapiente repellendus
-          eos.</span
-        >
+    <div v-if="index % 2 == 0" id="specials" class="h-auto mx-5 my-10">
+      <div class="flex flex-col lg:grid lg:grid-cols-2">
+        <div class="grid-start-1 flex flex-col items-center lg:border-r">
+          <span class="text-9xl">{{ dataObject.title }}</span>
+        </div>
+        <div class="grid-start-2 mx-5 flex flex-col h-48 items-center">
+          <span class="">{{ dataObject.shortDescription }}</span>
+          <button
+            class="m-auto rounded-full border w-auto px-3 py-2 hover:shadow-md"
+            :onClick="() => $router.push({ name: dataObject.link })"
+          >
+            Visit Page
+          </button>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  props: ["dataObject", "index"],
+  created() {
+    console.log(this.dataObject);
+  },
+};
 </script>
 
 <style></style>
