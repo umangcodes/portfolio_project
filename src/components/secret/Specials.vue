75db37cc775675e1613dfdb6f8906c925ff23330
Umang A <umanga.eng@gmail.com>
2022-01-12 09:13:26 -0500

    css changed to enhance user experince. The cards now have a shadow on hover and mobile view updated

--- src/components/secret/Specials.vue
+++ src/components/secret/Specials.vue
@@ -2,32 +2,38 @@
   <div>
     <div
       v-if="index % 2 != 0"
-      class="flex flex-col lg:grid lg:grid-cols-2 mt-40"
+      class="h-auto flex flex-col mx-5 my-10 hover:shadow-xl lg:grid lg:grid-cols-2 rounded-xl p-5"
     >
       <div class="grid-start-2 flex flex-col items-center lg:border-l">
-        <span class="text-9xl">{{ dataObject.title }}</span>
+        <span class="lg:text-9xl text-7xl">{{ dataObject.title }}</span>
       </div>
       <div
-        class="grid-start-1 row-start-1 mx-5 flex flex-col h-48 items-center"
+        class="grid-start-1 row-start-1 mx-5 flex flex-col h-48 items-center justify-center"
       >
-        <span class="text-center">{{ dataObject.shortDescription }}</span>
+        <span class="">{{ dataObject.shortDescription }}</span>
         <button
-          class="m-auto rounded-full border w-auto px-3 py-2 hover:shadow-md"
+          class="mt-5 rounded-full border w-auto px-3 py-2 hover:shadow-md"
           :onClick="() => $router.push({ name: dataObject.link })"
         >
           Visit Page
         </button>
       </div>
     </div>
-    <div v-if="index % 2 == 0" id="specials" class="h-auto mx-5 my-10">
+    <div
+      v-if="index % 2 == 0"
+      id="specials"
+      class="h-auto mx-5 my-10 hover:shadow-xl hover:border-2 rounded-xl p-5"
+    >
       <div class="flex flex-col lg:grid lg:grid-cols-2">
         <div class="grid-start-1 flex flex-col items-center lg:border-r">
-          <span class="text-9xl">{{ dataObject.title }}</span>
+          <span class="lg:text-9xl text-7xl">{{ dataObject.title }}</span>
         </div>
-        <div class="grid-start-2 mx-5 flex flex-col h-48 items-center">
+        <div
+          class="grid-start-2 mx-5 flex flex-col h-48 items-center justify-center"
+        >
           <span class="">{{ dataObject.shortDescription }}</span>
           <button
-            class="m-auto rounded-full border w-auto px-3 py-2 hover:shadow-md"
+            class="mt-5 rounded-full border w-auto px-3 py-2 hover:shadow-md"
             :onClick="() => $router.push({ name: dataObject.link })"
           >
             Visit Page
