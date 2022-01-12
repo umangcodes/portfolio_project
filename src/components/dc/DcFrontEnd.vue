0f351dfd6565994afc24ec74e58b35913b3679e4
Umang A <umanga.eng@gmail.com>
2022-01-12 00:29:40 -0500

    dc front end path created, card for channels created, data added

--- src/components/dc/DcFrontEnd.vue
+++ src/components/dc/DcFrontEnd.vue
@@ -1,11 +1,160 @@
 <template>
-  <div>
-    <span>Front end</span>
+  <div class="mx-5 my-5 min-h-screen">
+    <div id="front-end-JS-courses">
+      <span class="text-2xl mx-5 flex">
+        >> Amazing Resources for
+        <a
+          href="https://www.javascript.com/"
+          class="rounded-xl mx-1 px-3 py-1 bg-yellow-300 font-bold hover:shadow-lg"
+          >JS</a
+        >
+      </span>
+      <Cards
+        v-for="(object, index) in javascript"
+        :dataObject="object"
+        :key="index"
+      />
+    </div>
+    <div id="front-end-vue-courses">
+      <span class="text-2xl mx-5">
+        >> Amazing Resources for
+        <a
+          href="https://vuejs.org/"
+          class="font-bold text-green-500 hover:text-green-700"
+          >Vue.js</a
+        >
+      </span>
+      <Cards v-for="(object, index) in vue" :dataObject="object" :key="index" />
+    </div>
+    <div id="front-end-react-courses">
+      <span class="text-2xl mx-5">
+        >> Amazing Resources for
+        <a
+          href="https://reactjs.org/"
+          class="drop-shadow-lg font-bold text-blue-500 hover:text-blue-700 hover:text-shadow-lg"
+          >React.js</a
+        >
+      </span>
+      <Cards
+        v-for="(object, index) in react"
+        :dataObject="object"
+        :key="index"
+      />
+    </div>
+    <div id="front-end-related-courses">
+      <span class="text-2xl mx-5"> >> Related Resources </span>
+      <Cards
+        v-for="(object, index) in mongo"
+        :dataObject="object"
+        :key="index"
+      />
+    </div>
   </div>
 </template>
 
 <script>
-export default {};
+import Cards from "./frontEnd/cards/Channels.vue";
+export default {
+  components: {
+    Cards,
+  },
+  data() {
+    return {
+      javascript: [
+        {
+          id: 1,
+          channelTitle: "Traversy Media",
+          shortDesc:
+            "An amazing channel for beginners as well as refined web developers. Visit the channel for more info.",
+          creator: "Brad Traversy",
+          links: {
+            youtube: "https://www.youtube.com/c/TraversyMedia/",
+            site: "https://traversymedia.com/",
+          },
+          img: "https://yt3.ggpht.com/ytc/AKedOLSxHOOxxa9Af8Bfb2XMop3lm4tor9bViWiC-d5aaw=s88-c-k-c0x00ffffff-no-rj",
+          stars: 10,
+          speciality: ["Web dev", "All"],
+        },
+        {
+          id: 2,
+          channelTitle: "Web dev simplified",
+          shortDesc:
+            "One of the best channels to learn react and javascript overall. Visit the channel for more info.",
+          creator: "To be updated",
+          links: {
+            youtube: "https://www.youtube.com/c/WebDevSimplified",
+            site: "https://courses.webdevsimplified.com/",
+          },
+          img: "https://yt3.ggpht.com/ytc/AKedOLQpvSjzSCSo8ZKCjBZS7TRX7omb_kyQirh2zgEY=s88-c-k-c0x00ffffff-no-rj",
+          stars: 9,
+          speciality: ["Web dev", "All"],
+        },
+        {
+          id: 3,
+          channelTitle: "Free code camp",
+          shortDesc:
+            "Great place to start learning a majority of web development technologies. Visit the channel for more info.",
+          creator: "Quincy Larson",
+          links: {
+            youtube: "https://www.youtube.com/c/Freecodecamp",
+            site: "https://www.freecodecamp.org/",
+          },
+          img: "https://yt3.ggpht.com/ytc/AKedOLRyPFojwY3CXV5ks5TwPrt2VifQn-nYNfkgLvVPkw=s88-c-k-c0x00ffffff-no-rj",
+          stars: 9,
+          speciality: ["Web dev", "All"],
+        },
+      ],
+      vue: [
+        {
+          id: 1,
+          channelTitle: "Vue Mastery",
+          shortDesc:
+            "Want a starting point for becoming a Vue.js developer? Join thousands of developers here and learn from the creators of the technology directly.",
+          creator: "Brad Traversy",
+          links: {
+            youtube: "https://www.youtube.com/c/VueMastery",
+            site: "https://www.vuemastery.com/",
+          },
+          img: "https://yt3.ggpht.com/ytc/AKedOLTaTTjZNnrrDe78dmFGsvjpEmraDQ1jUTLG8_Up=s88-c-k-c0x00ffffff-no-rj",
+          stars: 10,
+          speciality: ["Web dev", "All"],
+        },
+      ],
+      react: [
+        {
+          id: 1,
+          channelTitle: "The Net Ninja",
+          shortDesc:
+            "Be a great web developer or should I say the net ninja? Be a part of the GANG! Train here on this channel.",
+          creator: "Shaun Pelling",
+          links: {
+            youtube: "https://www.youtube.com/c/TheNetNinja",
+            site: "https://netninja.dev/",
+          },
+          img: "https://yt3.ggpht.com/ytc/AKedOLT3v89U-2iVX-78hqPk1-lBIduTcljrKLIH9YJg1A=s88-c-k-c0x00ffffff-no-rj",
+          stars: 9,
+          speciality: ["Web dev", "All"],
+        },
+      ],
+      mongo: [
+        {
+          id: 1,
+          channelTitle: "MongoDB",
+          shortDesc:
+            "Learn the fundamentals to advanced topics in mongodb. This is the best resource available directly from the giant.",
+          creator: "mongodb org",
+          links: {
+            youtube: "https://www.youtube.com/user/MongoDB",
+            site: "https://university.mongodb.com/",
+          },
+          img: "https://yt3.ggpht.com/94s5L5iEC6TInISXIFzVaVCaFgL62lEmSz3c9p2AHnjv7kmNAOXdWrgyndV-jttIC31K7AWWJw=s88-c-k-c0x00ffffff-no-rj",
+          stars: 10,
+          speciality: ["Web dev", "All"],
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <style lang="scss" scoped></style>
---
+++ src/components/dc/frontEnd/cards/Channels.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="rounded-xl border shadow-lg w-auto min-h-fit px-5 pt-5 my-5">
+    <div id="card-title" class="">
+      <div class="grid grid-col-2">
+        <div class="lg:col-start-1">
+          <div class="text-thin text-2xl">
+            <!-- <span class="mx-2">{{ dataObject.id }}</span> -->
+            <span class="mx-2">{{ dataObject.channelTitle }}</span>
+          </div>
+        </div>
+        <div class="lg:col-start-2 flex lg:justify-self-end mx-2 lg:mr-10">
+          <div class="flex w-fit">
+            <div v-for="(star, index) in dataObject.stars" :key="index">
+              <img
+                class="h-5 w-5"
+                :src="require('@/assets/starFilled.png')"
+                alt=""
+              />
+            </div>
+            <div v-for="(star, index) in rating" :key="index">
+              <img class="h-5 w-5" :src="require('@/assets/star.png')" alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div
+      id="card-body"
+      class="lg:grid lg:grid-cols-4 grid grid-cols-2 mx-5 mt-5"
+    >
+      <div
+        id="card-image"
+        class="col-start-1 my-5 mx-10 flex items-center justify-center"
+      >
+        <img
+          :src="dataObject.img"
+          class="h-auto w-auto rounded-full"
+          alt="IMG"
+        />
+      </div>
+      <div id="card-desc" class="col-start-2 lg:col-span-3 my-5">
+        <div class="flex flex-col">
+          <span class="text-thin italic lg:text-xl">
+            {{ dataObject.shortDesc }}
+          </span>
+          <div id="card-actions" class="flex justify-end my-5">
+            <a
+              class="rounded-lg w-10 cursor-pointer"
+              :href="dataObject.links.youtube"
+            >
+              <img
+                class="h-8 w-8 flex flex-col justify-center items-center"
+                :src="require('@/assets/youtube.png')"
+                alt=""
+              />
+            </a>
+            <a
+              class="rounded-lg w-10 cursor-pointer"
+              :href="dataObject.links.site"
+            >
+              <img
+                class="h-7 w-7 flex flex-col justify-center items-center"
+                :src="require('@/assets/www.png')"
+                alt=""
+              />
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      stars: 0,
+    };
+  },
+  props: ["dataObject"],
+  computed: {
+    rating() {
+      return 10 - this.dataObject.stars;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
