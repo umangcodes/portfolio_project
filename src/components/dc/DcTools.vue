1b6340dbf52dd35ab76cff8d8bbfa356b76520e0
Umang A <umanga.eng@gmail.com>
2022-01-12 09:11:04 -0500

    added dc tools view to the project

--- src/components/dc/DcTools.vue
+++ src/components/dc/DcTools.vue
@@ -1,11 +1,116 @@
 <template>
-  <div>
-    <span>Tools</span>
+  <div class="mx-5 my-5 min-h-screen">
+    <div id="front-end-tools" class="">
+      <span class="text-2xl mx-5"> Front End</span>
+      <Tools v-for="tool in frontEndTools" :dataObject="tool" :key="tool.id" />
+    </div>
+    <div id="api-tools" class="">
+      <span class="text-2xl mx-5"> API Tools</span>
+      <Tools v-for="tool in apiTools" :dataObject="tool" :key="tool.id" />
+    </div>
+    <span class="flex justify-center italic text-lg font-thin"
+      >To be updated soon...</span
+    >
   </div>
 </template>
 
 <script>
-export default {};
+import Tools from "@/components/dc/frontEnd/cards/Tools.vue";
+export default {
+  data() {
+    return {
+      frontEndTools: [
+        {
+          id: 1,
+          toolName: "Flaticon",
+          shortDesc:
+            "Find png and svg icons for your projects. customize the size of the icons easily using different features on website",
+          link: "https://www.flaticon.com/",
+          img: "https://media.flaticon.com/dist/min/img/logo/flaticon_negative.svg",
+          category: {
+            frontEnd: true,
+            backEnd: false,
+            type: "UI/UX",
+            others: "",
+          },
+          free: "basic",
+          stars: 9,
+        },
+        {
+          id: 2,
+          toolName: "Coolors",
+          shortDesc:
+            "Having trouble finding the best theme and colors for your web page? Find a random palette of colors here.",
+          link: "https://coolors.co",
+          img: "https://coolors.co/assets/img/logo.svg",
+          category: {
+            frontEnd: true,
+            backEnd: false,
+            type: "UI/UX",
+            others: "",
+          },
+          free: "full",
+          stars: 7,
+        },
+        {
+          id: 3,
+          toolName: "MD Color tool",
+          shortDesc:
+            "Color selection tool based on the best practices define by Material Design.",
+          link: "https://material.io/resources/color/#!/?view.left=0&view.right=0",
+          img: "",
+          altImg: "Google",
+          category: {
+            frontEnd: true,
+            backEnd: false,
+            type: "UI/UX",
+            others: "",
+          },
+          free: "full",
+          stars: 8,
+        },
+        {
+          id: 3,
+          toolName: "MD Color tool",
+          shortDesc:
+            "One stop shop for all material design icons. Simple and efficient to use in both staging and production",
+          link: "https://materialdesignicons.com/",
+          img: "",
+          altImg: "MDI",
+          category: {
+            frontEnd: true,
+            backEnd: false,
+            type: "UI/UX",
+            others: "",
+          },
+          free: "full",
+          stars: 8,
+        },
+      ],
+      apiTools: [
+        {
+          id: 1,
+          toolName: "Postman",
+          shortDesc:
+            "One of the best api testing tools available. Visit the site to know more",
+          link: "https://www.postman.com/",
+          img: "https://www.vectorlogo.zone/logos/getpostman/getpostman-ar21.svg",
+          category: {
+            frontEnd: false,
+            backEnd: false,
+            type: "testing",
+            others: "",
+          },
+          free: "basic",
+          stars: 9,
+        },
+      ],
+    };
+  },
+  components: {
+    Tools,
+  },
+};
 </script>
 
 <style lang="scss" scoped></style>
