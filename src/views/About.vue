8435fcb54f876bd9e15a12541a374562025b853e
Umang A <umanga.eng@gmail.com>
2022-01-13 07:44:21 -0500

    about page title sticky. z index for header changed to 20

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,9 +1,13 @@
 <template>
   <div id="about" class="h-screen">
-    <div class="flex flex-col justify-center">
-      <span class="italic text-3xl font-thin text-center"
-        >About this portfolio</span
+    <div class="flex flex-col justify-center z-0">
+      <div
+        class="sticky top-24 bg-white bg-opacity-75 flex justify-center py-5 border"
       >
+        <span class="italic text-3xl font-thin text-center"
+          >About this portfolio</span
+        >
+      </div>
       <p class="text-lg font-extralight p-5 text-center">
         This site is still under development. Please visit again for more
         content.
