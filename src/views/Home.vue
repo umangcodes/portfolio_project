92b2a46ecac8e376738df5f3ef6c96607299500c
Umang A <umanga.eng@gmail.com>
2022-01-12 19:38:39 -0500

    newly created component imported and used

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,33 +2,10 @@
   <div class="h-auto">
     <Intro />
     <!-- cover image/ intro -->
-    <div class="flex flex-col lg:grid lg:grid-cols-2 mx-5 my-10 h-screen">
-      <!-- <img
-        class="object-cover h-50 w-full"
-        src="@/assets/cover-image.jpg"
-        alt=""
-      /> -->
-      <div class="flex lg:cols-start-1 px-16 py-10 items-center">
-        <div class="flex flex-col">
-          <p class="font-semibold text-2xl text-center py-10">My philosophy</p>
-          <div class="text-xl font-thin">
-            <ul
-              class="flex flex-col justify-center text-center lg:list-none list-disc"
-            >
-              <li>Deliver modern and visually appealing web applications</li>
-              <li>Give responsive design and mobile first high importance</li>
-              <li>Follow the principles of clean code and SOLID</li>
-              <li>Consider future compatibility</li>
-            </ul>
-          </div>
-        </div>
-      </div>
-      <div class="flex lg:cols-start-2 items-center">
-        <img src="../assets/cover-image.jpg" class="rounded-lg" alt="" />
-      </div>
-    </div>
+    <Philosophy />
     <!-- interests -->
     <!-- skills -->
+    <Dc />
     <SkillsV2 />
     <!-- projects -->
     <!-- blog -->
@@ -43,6 +20,9 @@ import Intro from "../components/home/Intro.vue";
 import SkillsV2 from "../components/home/SkillsV2.vue";
 import Updates from "../components/home/Updates.vue";
 import Login from "../components/navigation/Login.vue";
+import Dc from "../components/home/Dc.vue";
+import Philosophy from "../components/home/Philosophy.vue";
+
 export default {
   name: "Home",
   components: {
@@ -50,6 +30,8 @@ export default {
     SkillsV2,
     Updates,
     Login,
+    Dc,
+    Philosophy,
   },
 };
 </script>
