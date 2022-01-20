54a4abb41658682969c18b07744e1a3af0500752
Umang A <umanga.eng@gmail.com>
2022-01-19 21:43:09 -0500

    intro padding on mobile devices changed

--- src/components/home/Intro.vue
+++ src/components/home/Intro.vue
@@ -3,7 +3,7 @@
     <!-- class="flex h-screen items-center m-0" -->
     <div class="flex flex-col justify-center -my-24 h-screen">
       <span class="text-3xl text-center font-semibold">Hi, I'm Umang.</span>
-      <span class="text-center text-lg font-thin"
+      <span class="text-center text-lg font-thin px-5"
         >Web developer, Control systems Engineer, Aviation enthusiast and much
         more.
       </span>
