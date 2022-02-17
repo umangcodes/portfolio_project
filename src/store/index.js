c0b323aa4373b91cd378695e39903c394e3e79a7
Umang A <umanga.eng@gmail.com>
2022-02-16 19:14:07 -0500

    fixed the inital value of the passcode to avoid accidental access

--- src/store/index.js
+++ src/store/index.js
@@ -2,7 +2,7 @@ import { createStore } from "vuex";
 
 export default createStore({
   state: {
-    passcode: "Pass!",
+    passcode: "",
     personalInfo: {
       linkedin: "https://www.linkedin.com/in/umanga-eng/",
       github: "https://github.com/umangcodes",
